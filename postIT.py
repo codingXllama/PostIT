008c1d3ac504d2d326ce81698d7f71c1b2d61731
codingXllama <osamaalchalabi2@gmail.com>
2020-01-01 14:11:03 -0500

    random change

--- postIT.py
+++ postIT.py
@@ -1,38 +1,37 @@
-from flask import Flask,render_template,url_for
-app= Flask(__name__)
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
 
 
 # creating dummy data, which contains an array called 'posts' that holds  dictionary with key and value
-Jan_posts=[
+Jan_posts = [
     # Creating the 1st dictionary
     {
-        'author':"Osama A",
-        'title': "Happy New Years",
+        "author": "Osama A",
+        "title": "Happy New Years",
         "content": "Hope you have a wonderful new year and inshallah your dreams can come true :)",
-        "date_posted":"Jan 1, 2019"
+        "date_posted": "Jan 1, 2019",
     },
     # Creating the 2nd dictionary
     {
-        'author':"Nexa D.",
-        'title': "Cats",
+        "author": "Nexa D.",
+        "title": "Cats",
         "content": "I love playing with my cat",
-        "date_posted":"Jan 2, 2019"
+        "date_posted": "Jan 2, 2019",
     },
-    
 ]
-
 
 
 @app.route("/")
 @app.route("/home")
 def hello():
-    return render_template("home.html",Jan_posts=Jan_posts)
+    return render_template("home.html", Jan_posts=Jan_posts)
+
 
 @app.route("/about")
 def about():
-    return render_template("about.html",titleName="About")
+    return render_template("about.html", titleName="About")
 
 
-
-if __name__=="__main__": 
+if __name__ == "__main__":
     app.run(debug=True)

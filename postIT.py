378a61d0512ef3cd7a19abb88dea9b526886d6b0
codingXllama <osamaalchalabi2@gmail.com>
2019-12-31 23:24:31 -0500

    Created the basic template files, and their basic content

---
+++ postIT.py
@@ -0,0 +1,38 @@
+from flask import Flask,render_template
+app= Flask(__name__)
+
+
+# creating dummy data, which contains an array called 'posts' that holds  dictionary with key and value
+Jan_posts=[
+    # Creating the 1st dictionary
+    {
+        'author':"Osama A",
+        'title': "Blog TITLE 1",
+        "content": "First post content",
+        "date_posted":"Jan 1, 2019"
+    },
+    # Creating the 2nd dictionary
+    {
+        'author':"Sam X",
+        'title': "Blog TITLE 2",
+        "content": "second post content",
+        "date_posted":"Jan 2, 2019"
+    },
+    
+]
+
+
+
+@app.route("/")
+@app.route("/home")
+def hello():
+    return render_template("home.html",Jan_posts=Jan_posts)
+
+@app.route("/about")
+def about():
+    return render_template("about.html",titleName="About")
+
+
+
+if __name__=="__main__": 
+    app.run(debug=True)

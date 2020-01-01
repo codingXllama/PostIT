c80767e3467379695a1bd342761884febc325ea7
codingXllama <osamaalchalabi2@gmail.com>
2020-01-01 17:33:43 -0500

    created the login and registeration labels

--- forms.py
+++ forms.py
@@ -1,20 +1,36 @@
 from flask_wtf import FlaskForm
+
 # importing the required fields from the wtforms
-from wtforms import StringField,PasswordField,SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+
 # importing the required validators from the wtforms.validators
-from wtforms.validators import DataRequired, Length,Email,EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 # creating a regeisteration form
+# Creating the required fields for registering
+class RegisterationForm(FlaskForm):
 
-
-class regeisterationForm(FlaskForm):
     # the userName passed in into the StringField method is a 'label' that will be used with in the html form
     # creating the length of userName restriction as the DataRequired
     username = StringField(
         "Username", validators=[DataRequired(), Length(min=2, max=20)]
     )
-    
-    email= StringField("Email",validators=[DataRequired(),Email()])
-    password=PasswordField("password",validators=[DataRequired()])
-    confirm_password=PasswordField("Confirm Password",validators=[DataRequired(),EqualTo("password")])
+
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("password", validators=[DataRequired()])
+    confirm_password = PasswordField(
+        "Confirm Password", validators=[DataRequired(), EqualTo("password")]
+    )
+    # Creating the submit button field
+    submit = SubmitField("Sign Up")
+
+
+# Creating the LoginForm
+class LoginForm(FlaskForm):
+    # allowing the user to login with their email rather than userName
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("password", validators=[DataRequired()])
+    # allowing the user to stay logged in after the brower closes after some time, using a secure cookie
+    remember = BooleanField("Remember Me")
+    submit = SubmitField("Log In")
 
--- postIT.py
+++ postIT.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, url_for
 
+# importing the form classes from the form.py file since it's in the same directory
+from forms import RegisterationForm, LoginForm
+
 app = Flask(__name__)
 
-
+# Creating a secrete key to avoid cross platform attacks, modifying cookies,and other foreign attacks
+app.config["SECRET_KEY"] = "5b358dae34397f5e261e35f22d753c7a"
 # creating dummy data, which contains an array called 'posts' that holds  dictionary with key and value
 Jan_posts = [
     # Creating the 1st dictionary
@@ -33,5 +37,21 @@ def about():
     return render_template("about.html", titleName="About")
 
 
+# Creating the registeration route
+@app.route("/register")
+def register():
+    # Now we must create an instance of our form,hence we use '()'
+    registerationForm = RegisterationForm()
+    return render_template("register.html", title="Register", regForm=registerationForm)
+
+
+# Creating the login form route
+@app.route("/login")
+def login():
+    loginForm = LoginForm()
+    return render_template("login.html", title="Login", signInForm=loginForm)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
